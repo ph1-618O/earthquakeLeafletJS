716c3a5228f93ddf1f44793fa8e0720ed138863a
Jen S <j3n.s3idl@gmail.com>
2020-11-15 17:23:27 -0500

    working

---
+++ coordinateExp.js
@@ -0,0 +1,47 @@
+var coordinateExp = {
+    "type": "FeatureCollection",
+    "metadata": {
+        "generated": 1605460089000,
+        "url": "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson",
+        "title": "USGS All Earthquakes, Past Day",
+        "status": 200,
+        "api": "1.10.3",
+        "count": 315
+    },
+    "features": [{
+        "type": "Feature",
+        "properties": {
+            "mag": 2.3,
+            "place": "47 km S of Goldfield, Nevada",
+            "time": 1605459323160,
+            "updated": 1605459937543,
+            "tz": null,
+            "url": "https://earthquake.usgs.gov/earthquakes/eventpage/nn00784308",
+            "detail": "https://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/nn00784308.geojson",
+            "felt": null,
+            "cdi": null,
+            "mmi": null,
+            "alert": null,
+            "status": "automatic",
+            "tsunami": 0,
+            "sig": 81,
+            "net": "nn",
+            "code": "00784308",
+            "ids": ",nn00784308,",
+            "sources": ",nn,",
+            "types": ",origin,phase-data,",
+            "nst": 10,
+            "dmin": 0.97,
+            "rms": 0.33,
+            "gap": 189.24,
+            "magType": "ml",
+            "type": "earthquake",
+            "title": "M 2.3 - 47 km S of Goldfield, Nevada"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-117.2965, 37.2835, 8]
+        },
+        "id": "nn00784308"
+    }]
+}
--- logic2.js
+++ logic2.js
@@ -1,12 +1,14 @@
+//day is also fast
+var queryJson = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 //hour, is faster
 //var queryJson = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"
 //month for more detail, add options later
-var queryJson = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+// var queryJson = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 // from Class example
 // var queryJson = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
 //     "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
-function markerSize(mag){
+function markerSize(mag) {
     return mag * 30000;
 }
 
@@ -14,7 +16,7 @@ function markerSize(mag){
 function color(strength) {
     switch (true) {
         case strength > 5:
-            return "#ff7400";
+            return "#ff7400"; //"#ff0000"; 
         case strength > 4:
             return "#ff9a00";
         case strength > 3:
@@ -42,11 +44,13 @@ function createMap(quakeData) {
         },
         pointToLayer: function (feature, latlng) {
             return new L.circle(
-                    latlng, {
+                latlng, {
                     radius: markerSize(feature.properties.mag),
                     fillColor: color(feature.properties.mag),
                     fillOpacity: 0.5,
-                    stroke: false,
+                    color: 'white',
+                    weight: 0.5,
+                    stroke: true,
                 })
         }
     });
@@ -75,9 +79,17 @@ function buildMap(quakes) {
         accessToken: API_KEY
     });
 
+    var terrain = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",{
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.mapbox-terrain-v2",
+        accessToken: API_KEY
+    });
+
     var baseMap = {
         "Satellite Map": map,
-        "Dark Outlines": darkMap
+        "Dark Outlines": darkMap,
+        "Terrain": terrain
     };
     var overlayMap = {
         quakes,
@@ -101,13 +113,12 @@ function buildMap(quakes) {
         var div = L.DomUtil.create('div', 'info legend'),
             sizes = [0, 1, 2, 3, 4, 5];
 
-        for (var i = 0; i < sizes.length; i++) {
-            div.innerHTML +=
-                '<i style="background:' + color(sizes[i] + 1) + "></i>" + sizes[i] + (sizes[i + 1] ? ' - ' + sizes[i + 1] + '<br>' : ' + ');
-        }
+        // for (var i = 0; i < sizes.length; i++) {
+        //     div.innerHTML +=
+        //         '<i style="background:' + color(sizes[i] + 1) + "></i>" + sizes[i] + (sizes[i + 1] ? ' - ' + sizes[i + 1] + '<br>' : ' + ');
+        // }
         return div;
     };
 
     legend.addTo(stackLayers);
 }
-
