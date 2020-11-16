65a4f55288913f656d5ee989194ccf9a601c0ac8
Jen S <j3n.s3idl@gmail.com>
2020-11-15 23:03:45 -0500

    working on legend, fixed mag to depth

--- logic2.js
+++ logic2.js
@@ -35,18 +35,21 @@ d3.json(queryJson, function (mapData) {
 });
 
 
-
 //3
 function createMap(quakeData) {
+    // console.log(quakeData[2].properties.place);
+    // console.log(quakeData[0].geometry.coordinates);
+    // console.log(quakeData[0].geometry.coordinates[2]);
+
     var quakes = L.geoJson(quakeData, {
         onEachFeature: function (feature, layer) {
-            layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p> Magnitude" + feature.properties.mag + "</p>")
+            layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p> Magnitude: " + feature.properties.mag + "</p>" + "<p> Depth: " + feature.geometry.coordinates[2] + "</p>")
         },
         pointToLayer: function (feature, latlng) {
             return new L.circle(
                 latlng, {
                     radius: markerSize(feature.properties.mag),
-                    fillColor: color(feature.properties.mag),
+                    fillColor: color(feature.geometry.coordinates[2]),
                     fillOpacity: 0.5,
                     color: 'white',
                     weight: 0.5,
@@ -106,19 +109,42 @@ function buildMap(quakes) {
     }).addTo(stackLayers);
 
     var legend = L.control({
-        position: 'bottomleft'
+        position: 'bottomright'
     });
 
     legend.onAdd = function () {
-        var div = L.DomUtil.create('div', 'info legend'),
-            sizes = [0, 1, 2, 3, 4, 5];
+        console.log('working')
+        var div = L.DomUtil.create('div', 'info legend');
+            var sizes = [0, 1, 2, 3, 4, 5];
+            var colors = [
+            "#ff7400",
+            "#ff9a00",
+            "#ffb400",
+            "#4094b4",
+            "#ff0000",
+            "#0083c3"];
+            var labels = [];
+            // var i,  
 
-        // for (var i = 0; i < sizes.length; i++) {
-        //     div.innerHTML +=
-        //         '<i style="background:' + color(sizes[i] + 1) + "></i>" + sizes[i] + (sizes[i + 1] ? ' - ' + sizes[i + 1] + '<br>' : ' + ');
-        // }
+        var legendInfo = 
+                        "<h1>Earthquake Depth</h1>" + 
+                        "<div class=\"labels\">" +
+                        "<div class=\"min\">" + sizes[0] + "<div>" +
+                        "<div class=\"max\">" + sizes[sizes.length-1] + "</div>" +
+                        "<div>";
+        div.innerHTML = legendInfo;
+        sizes.forEach(function(size, index){
+            labels.push("<li style=\"background-color: " + colors[index] + "\"></li");
+        });
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
         return div;
+        // for (var i = 0; i < sizes.length; i++) {
+        //     console.log('???');
+            // div.innerHTML +=
+            //     "<i style='background: " + colors[i] + "'></i>" + sizes[i] + (sizes[i + 1] ? "&ndash;" + sizes[i + 1] + "<br>" : " + ");
+        }
+        // console.log(div);
+        legend.addTo(stackLayers);
+        
     };
 
-    legend.addTo(stackLayers);
-}
